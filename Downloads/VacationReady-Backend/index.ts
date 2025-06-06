2f7f3f59995b4b78f5d54cd34b13c54157538c90
Michael Dowdle <michael.dowdle@hotmail.com>
2025-06-06 09:08:22 +0100

    Add ping route for debug check

--- Downloads/VacationReady-Backend/index.ts
+++ Downloads/VacationReady-Backend/index.ts
@@ -7,6 +7,9 @@ app.use(express.json());
 app.use('/api', authRoutes);
 
 const PORT = process.env.PORT || 3000;
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
